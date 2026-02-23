53fc0d2ef2589896b7bbe9a1cb93c68c243527e6
elena.ryabova77m@gmail.com <elena.ryabova77m@gmail.com>
2026-02-23 10:25:05 +0300

    moved data about news and links to a folder data

---
+++ src/data/links.ts
@@ -0,0 +1,15 @@
+export interface Links {
+  href: string
+  title: string
+}
+
+export const linksIndex: Links[] = [
+  {
+    href: '/docs/availability.jpg',
+    title: 'Информация о доступности ТС для инвалидов и лиц с ограниченными возможностями'
+  },
+  {
+    href: '/docs/collectiveImmunity.pdf',
+    title: 'Паспорт коллективного иммунитета'
+  },
+]
