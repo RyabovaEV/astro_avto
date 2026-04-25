df366b596842e1aa8099707531e515a0e8a9f058
elena.ryabova77m@gmail.com <elena.ryabova77m@gmail.com>
2026-04-25 17:02:50 +0300

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  // build: {
-  //   inlineStylesheets: 'never'
-  // }
+  site: 'https://www.kpap.ru',
+  integrations: [sitemap()]
 });
