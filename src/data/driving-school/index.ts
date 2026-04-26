d92cc469cf4ee16a95b8aad78617d87f924575a0
elena.ryabova77m@gmail.com <elena.ryabova77m@gmail.com>
2026-04-26 15:19:34 +0300

    изменения в автошколе, документах и прочем

--- src/data/driving-school/index.ts
+++ src/data/driving-school/index.ts
@@ -4,13 +4,11 @@ export {dangerGoodsPrice} from './dangerGoodsPrice'
 export { documentSections } from './document';
 
 import {program_B} from './programs/program_B'
-import {program_BC} from './programs/program_BC'
 import {program_CD} from './programs/program_CD'
 import {program_BD} from './programs/program_BD'
 
 export const programsMap = {
   B: program_B,
-  BC: program_BC,
   BD: program_BD,
   CD: program_CD
 }
